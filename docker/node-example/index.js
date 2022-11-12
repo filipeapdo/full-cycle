37136a9fb9b1e58c20d942198812f9d2a58e9f92
filipeapdo <filipeaparecido@gmail.com>
2022-11-12 16:17:13 -0300

    feat: update node-example, docker-compose

--- docker/node-example/index.js
+++ docker/node-example/index.js
@@ -2,10 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+})
+
+conn.connect(function (err) {
+    if (err) throw err;
+    console.log('Connected!');
+    const sql = 'INSERT INTO people (name) VALUES ("Filipus");';
+    conn.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("1 record inserted!")
+    })
+});
+
 app.get('/', (req, res) => {
     res.send('<h1>Filipus\' here!</h1>')
-})
+});
 
 app.listen(port, () => {
     console.log('Rodando na porta ' + port)
-})
+});
